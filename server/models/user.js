6ccaae45d76414b7adb19f18593b3255387c1568
Charlie Nguyen <vawntanng@gmail.com>
2024-05-27 17:03:58 +0700

    [BE][ADD] ADD API RESET-PASSWORD AND NEW-PASSWORD

--- server/models/user.js
+++ server/models/user.js
@@ -14,6 +14,8 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    resetCode: String,
+    resetCodeExpiration: Date,
   },
   {
     timestamps: true,
