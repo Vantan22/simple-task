ed338bbd4df3441e5391854e86373cca1d3623c3
Charlie Nguyen <vawntanng@gmail.com>
2024-05-27 09:59:50 +0700

    [BE][ADD] ADD API LOGIN

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const secret = process.env.SECRET_KEY;
+
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -30,6 +33,49 @@ exports.signup = async (req, res, next) => {
     const savedUser = await user.save();
     res.status(201).json({ message: "User created!", userId: savedUser._id });
   } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email: email });
+    console.log(user);
+    if (!user) {
+      const error = new Error("A user with this email could not be found.");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const loadedUser = user;
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Wrong password!");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    // Generate token
+    const now = new Date();
+    const tomorrow = new Date(now);
+    tomorrow.setDate(now.getDate() + 1);
+    tomorrow.setHours(0, 0, 0, 0);
+    const expiresIn = Math.floor((tomorrow - now) / 1000);
+
+    const token = jwt.sign(
+      {
+        userId: loadedUser._id.toString(),
+      },
+      secret,
+      { expiresIn: expiresIn },
+    );
+    res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
     next(err);
   }
 };
